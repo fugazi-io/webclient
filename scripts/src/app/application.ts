dde44c1107ef44025e62cf57f022bf0163e50d76
Nitzan Tomer <nitzan.tomer@gmail.com>
2017-02-07 02:55:13 +0200

    changed version to 1.0.1

--- scripts/src/app/application.ts
+++ scripts/src/app/application.ts
@@ -11,7 +11,7 @@
 
 module fugazi.app {
 	export const version = {
-		code: "1.0.0",
+		code: "1.0.1",
 		name: "The Argument",
 		toString: function() {
 			return `${ this.name } (${ this.code })`;
