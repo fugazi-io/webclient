42cdfcc2e83267489e48785924e213486f400544
Nitzan Tomer <nitzan.tomer@gmail.com>
2017-02-19 00:40:23 +0200

    hopefully fixed #20 added Math.ceil to the computation and changed from `===` to `>=`. also updated the version of the "perfect-scrollbar" and removed usage of `ReactDOM.findDOMNode` (in favor of ref)

--- scripts/src/view/output.tsx
+++ scripts/src/view/output.tsx
@@ -45,6 +45,7 @@ module fugazi.view {
 	export type StyleChangeListener = () => void;
 	
 	export class OutputView extends View<OutputProperties, OutputState> {
+		private element: HTMLElement;
 		private shouldScrollBottom: boolean;
 
 		static defaultProps: OutputProperties = {
@@ -73,7 +74,7 @@ module fugazi.view {
 			var node = ReactDOM.findDOMNode(this);
 
 			if (this.shouldScrollBottom) {
-				node.scrollTop = node.scrollHeight
+				node.scrollTop = node.scrollHeight;
 			}
 
 			Ps.update(node);
@@ -130,7 +131,7 @@ module fugazi.view {
 				blocks.unshift(<li key="hello" className="hello">fugazi version { app.version.code }</li>);
 			}
 			
-			return <section className="output">
+			return <section className="output" ref={ element => this.element = element }>
 				<div className="spacer"></div>
 				<ol className="items">{ blocks }</ol>
 			</section>;
@@ -142,8 +143,7 @@ module fugazi.view {
 		}
 
 		private checkOverflow(): boolean {
-			var node: any = ReactDOM.findDOMNode(this);
-			return node.scrollTop + node.offsetHeight === node.scrollHeight;
+			return Math.ceil(this.element.scrollTop + this.element.offsetHeight) >= this.element.scrollHeight;
 		}
 	}
 
